28e2690393b80009eaa59a108ab1537c67c1e723
sinoz <ilyas.alfan.b@gmail.com>
2019-02-04 17:22:44 +0100

    Load configurations from a HOCON .conf configuration file

--- application/src/main/scala/io/unity/application/launch/UnityLauncher.scala
+++ application/src/main/scala/io/unity/application/launch/UnityLauncher.scala
@@ -12,19 +12,48 @@ import scalaz.zio.{App, IO}
   * @author Sino
   */
 object UnityLauncher extends App {
+  /** The [[Path]] to the main Unity configuration file. */
+  val pathToUnityConfig = Paths.get("resources/configs/unity.conf")
+
   override def run(args: List[String]) =
     launch
       .attempt
-      .map(_.fold(_ => ExitStatus.ExitWhenDone(1, 1.minute), _ => ExitStatus.ExitNow(0)))
+      .map(_.fold(failure => exitWhenDone, success => exitNow))
 
+  /** Returns the exit status for delayed system exit. */
+  private def exitWhenDone = ExitStatus.ExitWhenDone(code = 1, timeout = 1.minute)
+
+  /** Returns the exit status for immediate system exit. */
+  private def exitNow = ExitStatus.ExitNow(code = 0)
+
+  /** Sets up the application components, loads resources into memory and boots
+    * up the server to deal with requests. */
   private def launch: IO[Exception, Unit] =
     for {
-      cache         <- loadAssetStorage(Paths.get("resources/assets/oldschool/177/storage/"))
+      config        <- loadUnityConfig(pathToUnityConfig)
+      _             <- info(s"$config")
+
+      cache         <- loadAssetStorage(config.storagePath)
       archiveCount  <- IO.sync(cache.getArchiveCount)
 
-      _             <- putStrLn(s"$archiveCount")
+      _             <- info(s"$archiveCount")
     } yield ()
 
+  /** Attempts to load a [[UnityConfig]] from a local configuration
+    * file on disk. */
+  private def loadUnityConfig(path: Path) =
+    UnityConfig.load(path)
+
+  /** Attempts to synchronously load assets from the binary storage
+    * into memory. */
   private def loadAssetStorage(path: Path) =
     IO.sync(new Cache(FileStore.open(path.toFile)))
+
+  /** Logs the given info message. */
+  private def info(message: String) =
+    putStrLn(s"[INFO]:[Launch]: $message") // TODO swap with actual logger
+
+  /** Logs the given [[Throwable]] as a system failure. */
+  private def error(t: Throwable) =
+    putStrLn(s"[ERR]:[Launch]: Caught failure: $t") // TODO swap with actual logger
 }
--- build.sbt
+++ build.sbt
@@ -4,6 +4,7 @@ val commonSettings = Seq(
   scalaVersion := "2.12.8"
 )
 
+val typesafeConfigVersion = "1.3.3"
 val twitterUtilVersion = "19.1.0"
 val zioVersion = "0.6.0"
 val nettyVersion = "4.1.33.Final"
@@ -34,6 +35,7 @@ lazy val application = project("application")
       val main = Seq(
         "org.scalaz" %% "scalaz-zio" % zioVersion,
         "io.netty" % "netty-buffer" % nettyVersion,
+        "com.typesafe" % "config" % typesafeConfigVersion,
         "com.twitter" %% "util-collection" % twitterUtilVersion
       )
       
