c233da1cd084114c22a2a4af557c872597579fc0
sinoz <ilyas.alfan.b@gmail.com>
2019-02-05 13:27:58 +0100

    Add CombatLevel and corresponding test case

---
+++ domain/src/main/scala/io/unity/domain/model/CombatLevel.scala
@@ -0,0 +1,41 @@
+package io.unity.domain.model
+
+object CombatLevel {
+  /** Evaluates the given [[Vector]] of [[Skill]]s to compute a
+    * new [[CombatLevel]]. */
+  def evaluate(set: Vector[Skill]) = {
+    val attack = set(Skill.Attack).maxLevel.toValue
+    val strength = set(Skill.Strength).maxLevel.toValue
+    val defence = set(Skill.Defence).maxLevel.toValue
+    val hitpoints = set(Skill.Hitpoints).maxLevel.toValue
+    val prayer = set(Skill.Prayer).maxLevel.toValue
+    val ranged = set(Skill.Ranged).maxLevel.toValue
+    val magic = set(Skill.Magic).maxLevel.toValue
+
+    var combat = (((defence + hitpoints + prayer / 2) * 0.25) + 1).toInt
+
+    val melee: Double = (attack + strength) * 0.325
+    val ranger: Double = Math.floor(ranged * 1.5) * 0.325
+    val mager: Double = Math.floor(magic * 1.5) * 0.325
+
+    if (melee >= ranger && melee >= mager) {
+      combat += melee.toInt
+    } else if (ranger >= melee && ranger >= mager) {
+      combat += ranger.toInt
+    } else if (mager >= melee && mager >= ranger) {
+      combat += mager.toInt
+    }
+
+    CombatLevel(Level(combat))
+  }
+}
+
+/**
+  * A combat level is a calculation of all combat skills combined.
+  * @author Sino
+  */
+case class CombatLevel(private val level: Level) {
+  def reevaluate(set: Vector[Skill]) = CombatLevel.evaluate(set)
+
+  def toValue = level.toValue
+}
---
+++ domain/src/test/scala/io/unity/domain/model/CombatLevelSpec.scala
@@ -0,0 +1,26 @@
+package io.unity.domain.model
+
+import org.scalatest.{FlatSpec, Matchers}
+
+final class CombatLevelSpec extends FlatSpec with Matchers {
+  "A combat level" should "start at level 1 when all combat skills are level 1" in {
+    val skillSet = Vector.tabulate(7)(id => Skill(id, Level(1), Level(1), Experience(0D)))
+    val combatLevel = CombatLevel.evaluate(skillSet)
+
+    combatLevel should equal(CombatLevel(Level(1)))
+  }
+
+  "A combat level" should "cap at level 126 when all combat skills are level 99" in {
+    val skillSet = Vector.tabulate(7)(id => Skill(id, Level(99), Level(99), Experience(0D)))
+    val combatLevel = CombatLevel.evaluate(skillSet)
+
+    combatLevel should equal(CombatLevel(Level(126)))
+  }
+
+  "A combat level" should "only be affected by max levels and not by normal levels" in {
+    val skillSet = Vector.tabulate(7)(id => Skill(id, Level(1), Level(99), Experience(0D)))
+    val combatLevel = CombatLevel.evaluate(skillSet)
+
+    combatLevel should equal(CombatLevel(Level(126)))
+  }
+}
