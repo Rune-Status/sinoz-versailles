3021351ae566686c422a833ceb2ba0b8aefb9ac7
sinoz <ilyas.alfan.b@gmail.com>
2019-02-07 16:23:06 +0100

    Work on AssetStreamingHandler

--- application/src/main/scala/io/unity/application/component/middleware/handler/AssetStreamingHandler.scala
+++ application/src/main/scala/io/unity/application/component/middleware/handler/AssetStreamingHandler.scala
@@ -1,11 +1,87 @@
 package io.unity.application.component.middleware.handler
 
-import io.netty.channel.ChannelInboundHandlerAdapter
+import scala.concurrent.duration._
+import io.netty.channel.{ChannelHandlerContext, ChannelInboundHandlerAdapter}
+import io.netty.util.concurrent.ScheduledFuture
+import io.unity.application.command.{AcknowledgeClientStateChange, ApplyEncryptionToFolder, RequestAssetFolder}
+import io.unity.application.model.FolderRequest
+
+import scala.collection.mutable
 
 /**
-  * TODO
+  * The application logic handler that processes requests from the client
+  * to stream particular folder of assets.
+  *
+  * This implementation runs a job at an interval rate of 1 second. Requests
+  * that arrive are at first hand put into a queue depending on their urgency.
+  * The job entails polling from these queues of requests and preparing them
+  * for transport. Once prepared, they are ejected for message encoding before
+  * being sent off to the game client.
+  *
+  * The job only polls a limited amount of [[FolderRequest]]s. This is mainly
+  * for bandwidth control. Folders are during preparation partitioned into
+  * blocks of 512 bytes. This allows accumulation of a limited amount of
+  * data (which are to be ejected) and delaying the remaining amount for the
+  * next batch.
+  *
   * @author Sino
   */
 final class AssetStreamingHandler extends ChannelInboundHandlerAdapter {
-  // TODO
+  /** The rate at which requests for asset folders are polled and served. */
+  private val Rate = 1.second
+
+  /** The scheduled task that runs the poll job at an interval rate. */
+  private var pollTask: Option[ScheduledFuture[_]] = None
+
+  /** The queues of [[FolderRequest]]s. */
+  private val urgent = mutable.Queue[FolderRequest]()
+  private val regular = mutable.Queue[FolderRequest]()
+
+  /** Schedules the [[pollTask]] to run at an interval [[Rate]]. */
+  private def schedulePollTask(ctx: ChannelHandlerContext): Unit = {
+    pollTask = Some(ctx.channel().eventLoop().scheduleAtFixedRate(() => poll(ctx), 0, Rate.length, Rate.unit))
+  }
+
+  /** Cancels a running [[pollTask]]. */
+  private def cancelPollTask(): Unit = {
+    pollTask.foreach(_.cancel(false))
+    pollTask = None
+  }
+
+  /** Polls from the [[urgent]] and [[regular]] queues of [[FolderRequest]]s
+    * to select which ones are to be ejected for transport. */
+  private def poll(ctx: ChannelHandlerContext): Unit = {
+    // TODO
+  }
+
+  /** Conveys the given [[FolderRequest]] onto an appropriate
+    * queue of [[FolderRequest]]s. */
+  private def convey(request: FolderRequest, isUrgent: Boolean): Unit = {
+    if (isUrgent) {
+      urgent.enqueue(request)
+    } else {
+      regular.enqueue(request)
+    }
+  }
+
+  override def handlerAdded(ctx: ChannelHandlerContext): Unit = {
+    schedulePollTask(ctx)
+  }
+
+  override def handlerRemoved(ctx: ChannelHandlerContext): Unit = {
+    cancelPollTask()
+  }
+
+  override def channelRead(ctx: ChannelHandlerContext, msg: Any): Unit = {
+    msg match {
+      case RequestAssetFolder(archive, folder, isUrgent) =>
+        convey(FolderRequest(archive, folder), isUrgent)
+
+      case AcknowledgeClientStateChange(newState) =>
+        // TODO
+
+      case ApplyEncryptionToFolder(mask) =>
+        // TODO
+    }
+  }
 }
---
+++ application/src/main/scala/io/unity/application/model/FolderRequest.scala
@@ -0,0 +1,10 @@
+package io.unity.application.model
+
+/**
+  * A request for a particular kind of [[Folder]] stored within an [[Archive]].
+  * @author Sino
+  */
+case class FolderRequest(
+  archive: Archive,
+  folder: Folder
+)
