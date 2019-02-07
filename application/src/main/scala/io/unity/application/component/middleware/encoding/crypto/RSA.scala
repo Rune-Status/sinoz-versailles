8f5a0840d054540422fa7d56255f4832cbf6b793
sinoz <ilyas.alfan.b@gmail.com>
2019-02-07 13:41:12 +0100

    Add cryptographic functions specific to the middleware component

---
+++ application/src/main/scala/io/unity/application/component/middleware/encoding/crypto/RSA.scala
@@ -0,0 +1,19 @@
+package io.unity.application.component.middleware.encoding.crypto
+
+import io.netty.buffer.ByteBuf
+
+/** @author Sino */
+trait RSA {
+  final def rsaCrypt(in: ByteBuf)(exponent: BigInt, modulus: BigInt) = {
+    val inData = new Array[Byte](in.readableBytes())
+    in.readBytes(inData)
+
+    val inInt = BigInt(inData)
+    val outInt = inInt.modPow(exponent, modulus)
+
+    val outData = outInt.toByteArray
+    val out = in.alloc().buffer(outData.length)
+
+    out.writeBytes(outData)
+  }
+}
