06282e3b3c9b38c14f2d1750ad3784804b9b0795
sinoz <ilyas.alfan.b@gmail.com>
2019-02-08 22:28:27 +0100

    Add SpecialAttackEnergy and corresponding test cases

---
+++ domain/src/main/scala/io/unity/domain/model/SpecialAttackEnergy.scala
@@ -0,0 +1,24 @@
+package io.unity.domain.model
+
+object SpecialAttackEnergy {
+  /** The minimal and maximum amount of special attack energy percentages. */
+  val MinPercentage = 0
+  val MaxPercentage = 100
+
+  /** The default initial [[SpecialAttackEnergy]] level. */
+  val Default = SpecialAttackEnergy(percentage = 100)
+}
+
+/**
+  * The energy level of the special attack bar.
+  * @author Sino
+  */
+case class SpecialAttackEnergy(private val percentage: Int) extends AnyVal {
+  def drain(amount: Int) =
+    copy(percentage = Math.max(percentage - amount, SpecialAttackEnergy.MinPercentage))
+
+  def restore(amount: Int) =
+    copy(percentage = Math.min(percentage + amount, SpecialAttackEnergy.MaxPercentage))
+
+  def toPercentage = percentage
+}
