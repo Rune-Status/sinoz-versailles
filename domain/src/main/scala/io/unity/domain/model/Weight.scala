5c5385fdd9b2368ef4da74bc6e7d61cd29d75cc7
sinoz <ilyas.alfan.b@gmail.com>
2019-02-08 21:23:16 +0100

    Add Weight and corresponding test cases

---
+++ domain/src/main/scala/io/unity/domain/model/Weight.scala
@@ -0,0 +1,20 @@
+package io.unity.domain.model
+
+object Weight {
+  val Default = Weight(kilos = 0)
+}
+
+/**
+  * The weight of an actor on the game world. The weight of an actor may
+  * drastically reduce the performance of an actor. A player for instance,
+  * for instance may carry a lot of items and ingame and thus have
+  * its stamina reduced at a faster rate than usual when running.
+  * @author Sino
+  */
+case class Weight(private val kilos: Float) extends AnyVal {
+  def lose(amountInKilos: Float) = copy(kilos = kilos - amountInKilos)
+
+  def gain(amountInKilos: Float) = copy(kilos = kilos + amountInKilos)
+
+  def toKilos = kilos
+}
---
+++ domain/src/test/scala/io/unity/domain/model/WeightSpec.scala
@@ -0,0 +1,19 @@
+package io.unity.domain.model
+
+import org.scalatest.{FlatSpec, Matchers}
+
+final class WeightSpec extends FlatSpec with Matchers {
+  "Weight" should "be able to be lost" in {
+    val weight = Weight.Default
+    val lost = weight.lose(25)
+
+    lost should equal(Weight(-25))
+  }
+
+  "An actor's weight" should "be able to be gained" in {
+    val weight = Weight.Default
+    val gained = weight.gain(50)
+
+    gained should equal(Weight(50))
+  }
+}
