ad4687bd19f7950815d6042c6fc561509899b4d0
sinoz <ilyas.alfan.b@gmail.com>
2019-02-17 16:45:26 +0100

    Adjust test cases to the further refactored domain model

--- domain/src/test/scala/io/versailles/domain/model/EnergySpec.scala
+++ domain/src/test/scala/io/versailles/domain/model/EnergySpec.scala
@@ -5,14 +5,14 @@ import org.scalatest.{FlatSpec, Matchers}
 final class EnergySpec extends FlatSpec with Matchers {
   "Adding units to an energy level" should "not exceed 100%" in {
     val initial = Energy(8000)
-    val updated = initial + 3000
+    val updated = initial + Energy(3000)
 
     updated.toPercentages should equal(100)
   }
 
   "Removing units from an energy level" should "not exceed 0%" in {
     val initial = Energy.MaxUnits
-    val updated = initial - 12000
+    val updated = initial - Energy(12000)
 
     updated.toPercentages should equal(0)
   }
--- domain/src/test/scala/io/versailles/domain/model/WeightSpec.scala
+++ domain/src/test/scala/io/versailles/domain/model/WeightSpec.scala
@@ -5,14 +5,14 @@ import org.scalatest.{FlatSpec, Matchers}
 final class WeightSpec extends FlatSpec with Matchers {
   "Weight" should "be able to be lost" in {
     val weight = Weight.Default
-    val lost = weight.lose(25)
+    val lost = weight.lose(Weight(25))
 
     lost should equal(Weight(-25))
   }
 
   "An actor's weight" should "be able to be gained" in {
     val weight = Weight.Default
-    val gained = weight.gain(50)
+    val gained = weight.gain(Weight(50))
 
     gained should equal(Weight(50))
   }
